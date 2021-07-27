aa2909111c46dfc8c264c7a50a0276c0c07817f0
perillamint <perillamint@silicon.moe>
2021-07-28 03:36:30 +0900

    Declare lifetime for each renderer

--- src/kanakanji/fontx.rs
+++ src/kanakanji/fontx.rs
@@ -1,19 +1,19 @@
 use crate::common::{CharacterRenderer, RenderFailureReason};
 
-pub struct FONTX {
-    //
+pub struct FONTX<'a> {
+    rom: &'a[u8],
 }
 
-impl CharacterRenderer for FONTX {
+impl CharacterRenderer for FONTX<'_> {
     fn render(&self, character: char, buf: &mut [u8]) -> Result<(usize, usize), RenderFailureReason> {
         todo!()
     }
 }
 
-impl FONTX {
-    pub fn new() -> FONTX {
+impl<'a> FONTX<'a> {
+    pub fn new(rom: &'a[u8]) -> FONTX {
         FONTX {
-            //
+            rom,
         }
     }
 }
--- src/latin/fnt.rs
+++ src/latin/fnt.rs
@@ -1,19 +1,21 @@
 use crate::common::{CharacterRenderer, RenderFailureReason};
 
-pub struct FNT {
+pub struct FNT<'a> {
+    rom: &'a[u8],
     width: usize,
     height: usize,
 }
 
-impl CharacterRenderer for FNT {
+impl CharacterRenderer for FNT<'_> {
     fn render(&self, character: char, buf: &mut [u8]) -> Result<(usize, usize), RenderFailureReason> {
         todo!()
     }
 }
 
-impl FNT {
-    pub fn new(width : usize, height: usize) -> FNT {
+impl<'a> FNT<'a> {
+    pub fn new(rom: &'a[u8], width : usize, height: usize) -> FNT {
         FNT {
+            rom,
             width,
             height,
         }
